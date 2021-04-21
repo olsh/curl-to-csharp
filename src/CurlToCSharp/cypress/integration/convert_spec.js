5913e07f4774060c8bffbd1624962fbabcaac993
Oleg Shevchenko <olsh.me@gmail.com>
2021-04-22 00:36:07 +0300

    Improve cypress tests

--- src/CurlToCSharp/cypress/integration/convert_spec.js
+++ src/CurlToCSharp/cypress/integration/convert_spec.js
@@ -1,37 +1,43 @@
-﻿// ReSharper disable StatementTermination
+// ReSharper disable StatementTermination
 // ReSharper disable UseOfImplicitGlobalInFunctionScope
 describe('Convert', function() {
     beforeEach(() => {
         cy.visit("/")
+        cy.setConvertIntercept()
     })
 
     it('Valid curl command works', function() {
         cy.get('#curl').type('curl -X POST github.com --compressed')
         cy.contains('Convert').click()
+        cy.waitConvertResponse()
 
         cy.convertAssert()
     })
 
     it('GET example works', function() {
         cy.contains('GET').click()
+        cy.waitConvertResponse()
 
         cy.convertAssert()
     })
 
     it('POST example works', function() {
         cy.contains('POST').click()
+        cy.waitConvertResponse()
 
         cy.convertAssert()
     })
 
     it('POST Form example works', function() {
         cy.contains('POST Form').click()
+        cy.waitConvertResponse()
 
         cy.convertAssert()
     })
 
     it('Upload Files example works', function() {
         cy.contains('Upload Files').click()
+        cy.waitConvertResponse()
 
         cy.convertAssert()
     })
@@ -39,6 +45,7 @@ describe('Convert', function() {
     it('Warning shown when curl contains unknown parameter', function() {
         cy.get('#curl').type('curl github.com -v')
         cy.contains('Convert').click()
+        cy.waitConvertResponse()
 
         cy.get('#csharp').should('be.visible')
         cy.get('#warnings').should('be.visible')
--- src/CurlToCSharp/cypress/support/commands.js
+++ src/CurlToCSharp/cypress/support/commands.js
@@ -3,3 +3,11 @@ Cypress.Commands.add('convertAssert', () => {
     cy.get('#errors').should('not.be.visible')
     cy.get('#warnings').should('not.be.visible')
 })
+
+Cypress.Commands.add('setConvertIntercept', () => {
+    cy.intercept('/convert').as('convert')
+})
+
+Cypress.Commands.add('waitConvertResponse', () => {
+    cy.wait('@convert')
+})
